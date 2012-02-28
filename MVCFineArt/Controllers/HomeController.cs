6f7d9808f223f445f0cfb58f87e648e1421d6a0f
Mark Becker <mark_becker@comcast.net>
2012-02-27 22:09:53 -0800

    added project files

---
+++ MVCFineArt/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFineArt.Models;
+
+namespace MVCFineArt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to MVC Fine Art";
+
+            var model = new Artist()
+            {
+                FirstName = "Mark",
+                LastName = "Becker",
+                Medium = "Photograhy",
+                ExampleImage = "../../Content/Images/mbec1_22_v1_med.jpg"
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCFineArt/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFineArt.Models
+{
+    public class Artist
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Medium { get; set; }
+        public string ExampleImage { get; set; }
+    }
+}
